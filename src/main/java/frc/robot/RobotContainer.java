11e9dd3c8a55df2a2a84e362e9ad3d9d948d7540
Stoopipoopy <anotherguywithabeanie@gmail.com>
2023-08-20 16:28:15 -0700

    khols

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,8 +6,16 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Subsystems.DrivetrainSubsystem;
 
 public class RobotContainer {
+  CommandXboxController controller = new CommandXboxController(0);
+
+  DrivetrainSubsystem drivetrainSubsystem = new DrivetrainSubsystem();
+
+
+  
   public RobotContainer() {
     configureBindings();
   }
